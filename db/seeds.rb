574fa495972ee3f4bb1f65877f6129276f1ce410
Claudiuts <sarb.claudiu391@gmail.com>
2020-06-12 11:32:44 -0500

    cc2

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,9 +13,15 @@ vendors = ["Insomnia Cookies", "Cookies Cream","Carvel","Gregory's Coffee","Duan
 sweets = ["Chocolate Chip Cookie", "Chocolate Chunk Cookie","M&Ms Cookie","White Chocolate Cookie","Brownie","Peanut Butter Icecream Cake", ]
 
 vendors.each do |vendor|
-  Vendor.create(name: vendor)
+  Vendor.create!(name: vendor)
 end
 
 sweets.each do |sweet|
-  Sweet.create(name: sweet)
+  Sweet.create!(name: sweet)
 end
+
+vend_sweet1 = VendorSweet.new(vendor_name: "Insomnia Cookies", sweet_name: "Chocolate Chunk Cookie", price: 100)
+vend_sweet2 = VendorSweet.new(vendor_name: "Cookies Cream", sweet_name: "White Chocolate Cookie", price: 30)
+vend_sweet3 = VendorSweet.new(vendor_name: "Gregory's Coffee", sweet_name: "M&Ms Cookie", price: 10)
+vend_sweet4 = VendorSweet.new(vendor_name: "Carvel", sweet_name: "Peanut Butter Icecream Cake", price: 99)
+
